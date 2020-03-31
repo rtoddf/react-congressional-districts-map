1fdeb14b902d0f36d9d6c0a1da4d15f53bef7a54
Fleeman <tfleeman@TCHA0W10RPT14.cmg.int>
2020-03-31 12:30:57 -0400

    got hacky map to work

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import CongressionalDistricts from './CongressionalDistricts';
 import './app.css';
 
 class App extends React.Component {
     render(){
         return (
-            <h1>Hey there</h1>
+            <svg width="960" height="600">
+                <CongressionalDistricts width={960} height={600} />
+            </svg>
+            
         )
     }
 }
